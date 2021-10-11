f40fd45410ee47efa8df72c2ef74582d27492821
szemibalint <szemibalint@gmail.com>
2021-10-11 21:28:28 +0200

    created the projects and dependencies

---
+++ G1GLK1_HFT_2021221/G1GLK1_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace G1GLK1_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ G1GLK1_HFT_2021221/G1GLK1_HFT_2021221.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace G1GLK1_HFT_2021221.Repository
+{
+    public class Class1
+    {
+    }
+}
