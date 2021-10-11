7c19a6217af66806e0664e5b54dc09b3d62ed36b
szemibalint <szemibalint@gmail.com>
2021-10-11 21:43:30 +0200

    nuget packages added also the mdf, ldf files

--- G1GLK1_HFT_2021221/G1GLK1_HFT_2021221.Data/Class1.cs
+++ G1GLK1_HFT_2021221/G1GLK1_HFT_2021221.Data/Class1.cs
@@ -4,5 +4,6 @@ namespace G1GLK1_HFT_2021221.Data
 {
     public class Class1
     {
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True; MultipleActiveResultSets=True
     }
 }
