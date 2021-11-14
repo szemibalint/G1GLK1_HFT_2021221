b62aa9ee1049fc282f38252715807b3682507447
szemibalint <szemibalint@gmail.com>
2021-11-14 17:05:12 +0100

    mdf, ldf

--- G1GLK1_HFT_2021221.Data/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace G1GLK1_HFT_2021221.Data
-{
-    public class Class1
-    {
-        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True; MultipleActiveResultSets=True
-    }
-}
