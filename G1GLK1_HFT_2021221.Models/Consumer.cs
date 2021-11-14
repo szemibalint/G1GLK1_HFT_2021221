cf291f1e7d1b79234c80bb56a24a242021a74b01
szemibalint <szemibalint@gmail.com>
2021-11-14 17:05:36 +0100

    Consumer class

--- G1GLK1_HFT_2021221.Models/Consumer.cs
+++ G1GLK1_HFT_2021221.Models/Consumer.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace G1GLK1_HFT_2021221.Models
 {
-    class Consumer
+    [Table("Consumers")]
+    public class Consumer
     {
+        [Key]
+        public int ConsumerId  { get; set; }
+
+        [MaxLength(40)]
+        public string FirstName { get; set; }
+        [MaxLength(40)]
+        public string LastName { get; set; }
+        [MaxLength(69)]
+        public string Address { get; set; }
+        [NotMapped]
+        public virtual  ICollection<Order> Orders { get; set; }
     }
 }
