4ac4e530ead766c26dd6dce808387364d378616f
szemibalint <szemibalint@gmail.com>
2021-11-14 17:05:50 +0100

    Restaurant Class

--- G1GLK1_HFT_2021221.Models/Restaurant.cs
+++ G1GLK1_HFT_2021221.Models/Restaurant.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace G1GLK1_HFT_2021221.Models
 {
-    class Restaurant
+    [Table("Restaurants")]
+    public class Restaurant
     {
+        [Key]
+        public int RestaurantId { get; set; }
+
+        public string NameOfRestaurant { get; set; }
+
+        public string Location { get; set; }
+
+        public string Cuisine { get; set; }
+        [NotMapped]
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
