be6a8baf9bf45f4f5f2a36b6d75b81ae22bb2fce
szemibalint <szemibalint@gmail.com>
2021-11-14 17:05:43 +0100

    Order Class

---
+++ G1GLK1_HFT_2021221.Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1GLK1_HFT_2021221.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set;}
+
+        public DateTime TimeOfOrder { get; set; }
+
+        public string Food { get; set; }
+
+        public int Price { get; set; }
+
+        [ForeignKey("Restaurants")]
+        public int RestaurantId { get; set; }
+
+        [ForeignKey("Consumers")]
+        public int ConsumerId { get; set; }
+
+        [NotMapped]
+        public virtual Consumer Consumer { get; set; }
+        [NotMapped]
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
