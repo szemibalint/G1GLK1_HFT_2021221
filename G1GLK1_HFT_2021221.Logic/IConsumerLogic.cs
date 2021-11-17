695934d12ae3368be7ec3129e5cca396be229260
szemibalint <szemibalint@gmail.com>
2021-11-17 13:20:06 +0100

    Logic interfaces

---
+++ G1GLK1_HFT_2021221.Logic/IConsumerLogic.cs
@@ -0,0 +1,24 @@
+﻿using G1GLK1_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1GLK1_HFT_2021221.Logic
+{
+    public interface IConsumerLogic
+    {
+        public void CreateConsumer(Consumer consumer);
+
+        public Consumer GetConsumer(int consumerID);
+
+        public List<Consumer> GetConsumers();
+
+        public void DeleteConsumer(int consumerID);
+
+        public void UpdateName(int consumerID, string name);
+        public void UpdateAdress(int consumerID, string address);
+
+    }
+}
