01c0fcaecb713411ad531a374f6feeeec193c426
szemibalint <szemibalint@gmail.com>
2021-12-08 17:46:08 +0100

    endpoint created

--- G1GLK1_HFT_2021221.Endpoint/Startup.cs
+++ G1GLK1_HFT_2021221.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using G1GLK1_HFT_2021221.Data;
+using G1GLK1_HFT_2021221.Logic;
+using G1GLK1_HFT_2021221.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,15 @@ namespace G1GLK1_HFT_2021221.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddTransient<IConsumerLogic, ConsumerLogic>();
+            services.AddTransient<IOrderLogic, OrderLogic>();
+            services.AddTransient<IRestaurantLogic, RestaurantLogic>();
+            services.AddTransient<IConsumerRepository, ConsumerRepository>();
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            services.AddTransient<IRestaurantRepository, RestaurantRepository>();
+            services.AddTransient<Delivery_service_context, Delivery_service_context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +42,7 @@ namespace G1GLK1_HFT_2021221.Endpoint
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
